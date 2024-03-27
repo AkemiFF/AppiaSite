8d6f662eaa8022c997e2c29244a16d28a9aa8f5c
Brad <mirado.akemi@gmail.com>
2024-03-27 22:34:09 +0200

    front update

--- Front/views.py
+++ Front/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"index.html")
+
+def services(request):
+    return render(request,"services.html")
+
+def apropos(request):
+    return render(request,"apropos.html")
+
+def recrutement(request):
+    return render(request,"recrutement.html")
+
+def contact(request):
+    return render(request,"contact.html")
